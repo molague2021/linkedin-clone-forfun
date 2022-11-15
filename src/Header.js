7f042d8fbbd86d4f5209fc3f57b45ac86c277597
Martin Olague <molague23@hotmail.com>
2022-11-14 23:26:42 -0700

    Created the Header for the Linkin using MUI for Icons.

---
+++ src/Header.js
@@ -0,0 +1,42 @@
+import HeaderOption from './HeaderOption';
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+          alt=""
+        />
+
+        <div className="header__search">
+          {/*Search Icon */}
+          <SearchIcon />
+
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://media-exp1.licdn.com/dms/image/C5603AQHBsn8jJM_OgA/profile-displayphoto-shrink_100_100/0/1659114015563?e=1674086400&v=beta&t=pIvlQJc__o_ACmGXtfSuBngkk99IqBx1ol3260eLges"
+          title="me"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
